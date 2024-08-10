89ea4dc1d6d12c56531ffa02faa4d2cdfb51f0e6
binamralamsal <binamralamsal@gmail.com>
2024-08-11 00:48:58 +0545

    Removed db url from env

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,5 @@ export const env = z
   .object({
     BOT_TOKEN: z.string().min(1, { message: "BOT_TOKEN is required" }),
     NODE_ENV: z.enum(["development", "production"]).default("development"),
-    DATABASE_URL: z.string().min(1, { message: "DATABASE_URL is required" }),
   })
   .parse(process.env);
